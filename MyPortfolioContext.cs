732436d5ecaa3ad52d73a7ae294bd5b93d0ff0ca
Bilge Sena KANBUR <88281870+bilgekanbur@users.noreply.github.com>
2024-05-03 23:05:32 +0300

    bilge
    controller klasörü

---
+++ MyPortfolioContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    //dbcontext sınıfında nmiras al
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SENA; Initial Catalog=MyPortfolioDb ; Integrated Security=True");
+            
+        
+        
+        
+        }
+        //ilgili sınıflar property olarak geçirildi
+        //veri tabanında tablo oluşacak
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages{ get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias{ get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
